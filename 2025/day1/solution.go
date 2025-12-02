307483b3191edd38be653d686aa987d8c6deacec
erlendtorsvik <erlendtorsvik@hotmail.com>
2025-12-02 19:50:54 +0100

    day1part2 go modulo hates me

--- 2025/day1/solution.go
+++ 2025/day1/solution.go
@@ -16,12 +16,14 @@ func main() {
 	dialNumber := 50
 
 	password := part1(f, dialNumber)
+	password2 := part2(f, dialNumber)
 
-	fmt.Println("password:", password)
+	fmt.Println("password part1:", password)
+	fmt.Println("password part2:", password2)
 }
 
 func part1(f []byte, dialNumber int) int {
-	password := 0
+	var password int
 
 	for v := range strings.SplitSeq(strings.TrimSpace(string(f)), "\n") {
 		direction := v[0]
@@ -39,6 +41,55 @@ func part1(f []byte, dialNumber int) int {
 		if dialNumber%100 == 0 {
 			password++
 		}
+
+	}
+
+	return password
+}
+
+func part2(f []byte, dialNumber int) int {
+	var password int
+
+	for v := range strings.SplitSeq(strings.TrimSpace(string(f)), "\n") {
+		dialNumber %= 100
+
+		// Go fant ut en dritkul ting at de beholder fortegnet :))))))))))))))))))))))))
+		// Så -18 modulo 100 = -18, ikkje 82
+		// VERY COOL!!!!!
+		if dialNumber < 0 {
+			dialNumber += 100
+		}
+
+		direction := v[0]
+		number, err := strconv.Atoi(v[1:])
+		if err != nil {
+			panic("klarte ikkje konvertere??")
+		}
+
+		if number == 0 {
+			continue
+		}
+
+		if direction == 'L' {
+			number *= -1
+		}
+
+		adder := 100
+		if dialNumber == 0 {
+			adder = 0
+		}
+
+		dialNumber += number
+
+		var turns int
+
+		if dialNumber < 1 {
+			turns = (dialNumber*-1 + adder) / 100
+		} else {
+			turns = dialNumber / 100
+		}
+
+		password += turns
 
 	}
 
