1010862a9e148e68aafaefc1b8a119f29462e18a
erlendtorsvik <erlendtorsvik@hotmail.com>
2025-12-23 03:47:00 +0100

    node struct doesnt really help anything, allocates more memory and takes slightly more time

--- 2025/day4/solution.go
+++ 2025/day4/solution.go
@@ -1,23 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
-	"strings"
 	"time"
 )
-
-type node struct {
-	value     int
-	up        *node
-	upright   *node
-	upleft    *node
-	down      *node
-	downright *node
-	downleft  *node
-	right     *node
-	left      *node
-}
 
 var valueMap = map[rune]int{
 	'@': 1, '.': 0,
@@ -33,78 +21,56 @@ func main() {
 	fmt.Println("part2", part2(f))
 }
 
-func createNodeGrid(f []byte) [][]node {
-	lines := strings.Split(strings.TrimSpace(string(f)), "\n")
+func createNodeGrid(f []byte) [][]int {
+	lines := bytes.Split(bytes.TrimSpace(f), []byte{'\n'})
 
 	height := len(lines)
 	width := len(lines[0])
 
-	grid := make([][]node, height+1)
-	grid[0] = make([]node, width+2)
+	grid := make([][]int, height+1)
+	grid[0] = make([]int, width+2)
 
 	for i := 1; i < len(grid); i++ {
-		grid[i] = make([]node, width+2)
+		grid[i] = make([]int, width+2)
 
 		for j := 1; j < len(grid[0])-1; j++ {
 			c := rune(lines[i-1][j-1])
-			var n node
-			n.value = valueMap[c]
+			n := valueMap[c]
 			grid[i][j] = n
 		}
 
 	}
-	grid = append(grid, make([]node, width+2))
-
-	linkNodes(grid)
+	grid = append(grid, make([]int, width+2))
 
 	return grid
 }
 
-func linkNodes(grid [][]node) {
-	for r := 1; r < len(grid)-1; r++ {
-		for c := 1; c < len(grid[0])-1; c++ {
-			n := &grid[r][c]
-
-			n.up = &grid[r-1][c]
-			n.upleft = &grid[r-1][c-1]
-			n.upright = &grid[r-1][c+1]
-			n.down = &grid[r+1][c]
-			n.downleft = &grid[r+1][c-1]
-			n.downright = &grid[r+1][c+1]
-			n.left = &grid[r][c-1]
-			n.right = &grid[r][c+1]
-
-		}
-	}
-}
-
-func checkValidNodes(grid [][]node, rm bool) int {
+func checkValidNodes(grid [][]int, rm bool) int {
 	validNodes := 0
 
 	height := len(grid)
 	width := len(grid[0])
 	for r := 1; r < height-1; r++ {
 		for c := 1; c < width-1; c++ {
-			n := &grid[r][c]
-			if n.value == 0 {
+			if grid[r][c] == 0 {
 				continue
 			}
 
 			score := 0
 
-			score += n.up.value
-			score += n.upleft.value
-			score += n.upright.value
-			score += n.down.value
-			score += n.downleft.value
-			score += n.downright.value
-			score += n.left.value
-			score += n.right.value
+			score += grid[r-1][c]
+			score += grid[r-1][c-1]
+			score += grid[r-1][c+1]
+			score += grid[r+1][c]
+			score += grid[r+1][c-1]
+			score += grid[r+1][c+1]
+			score += grid[r][c-1]
+			score += grid[r][c+1]
 
 			if score < 4 {
 				validNodes++
 				if rm {
-					n.value = 0
+					grid[r][c] = 0
 				}
 			}
 		}
